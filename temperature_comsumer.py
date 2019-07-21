edbc2f5b6a0022862c62b1278b67c5ac84232cef
JonnyKog <jonnykong1996@gmail.com>
2019-07-20 22:13:50 -0700

    Add temperature producer and consumer

---
+++ temperature_comsumer.py
@@ -0,0 +1,65 @@
+import asyncio
+import logging
+import random
+import time
+from pyndn import Face, Name, Data, Interest, Blob
+from pyndn.security import KeyChain
+from asyncndn import fetch_data_packet
+
+
+class TemperatureConsumer(object):
+    def __init__(self, prefix: Name):
+        self.face = Face()
+        self.keychain = KeyChain()
+        self.running = True
+        self.prefix = prefix
+        self.face.setCommandSigningInfo(self.keychain, self.keychain.getDefaultCertificateName())
+        self.face.registerPrefix(self.prefix, None,
+                                 lambda prefix: logging.error("Prefix registration failed: %s", prefix))
+        
+        event_loop = asyncio.get_event_loop()
+        event_loop.create_task(self.face_loop())
+    
+    async def face_loop(self):
+        while self.running:
+            self.face.processEvents()
+            await asyncio.sleep(0.001)
+    
+    async def send_temp_interest(self):
+        interest_name = Name(self.prefix).append(str(int(time.time())))
+        interest = Interest(interest_name)
+
+        logging.info('Fetching {}'.format(str(interest.getName())))
+
+        data = await fetch_data_packet(self.face, interest)
+        if isinstance(data, Data):
+            self.process_temp_data(data)
+        else:
+            logging.info('Failed to fetch {}'.format(str(interest.getName())))
+    
+    def process_temp_data(self, data: Data):
+        content_bytes = data.getContent().toBytes()
+        temperature = int.from_bytes(content_bytes, byteorder='little')
+        logging.info('Received {}: {} degrees'.format(str(data.getName()), temperature))
+    
+    async def run(self):
+        """
+        Periodically send interest for temperature info
+        """
+        while self.running:
+            await self.send_temp_interest()
+            await asyncio.sleep(1)
+
+def main():
+    prefix = Name('/home')
+    sensor = TemperatureConsumer(prefix)
+
+    event_loop = asyncio.get_event_loop()
+    event_loop.run_until_complete(sensor.run())
+
+
+if __name__== "__main__":
+    logging.basicConfig(format='[%(asctime)s]%(levelname)s:%(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.INFO)
+    main()
